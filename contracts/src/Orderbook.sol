3cb7012176cf9073861b841829c9d9e97626435b
Yash Sharma <iamyxsh@gmail.com>
2024-09-21 05:02:21 +0800

    match order wip....

--- contracts/src/Orderbook.sol
+++ contracts/src/Orderbook.sol
@@ -3,18 +3,28 @@ pragma solidity ^0.8.13;
 
 import {Order} from "./utils/Types.sol";
 import {console} from "forge-std/console.sol";
+import {LiquidityManager} from "./LiquidityManager.sol";
+import {MockERC20} from "./mocks/MockERC20.sol";
+import {Order, LiquidityGroup, LiquidityToken} from "./utils/Types.sol";
 
 // --------- Errors ---------
 
+error Orderbook__InvalidMatching();
+error Orderbook__TokenTransferFailed();
+
 contract Orderbook {
     // --------- State ---------
+
+    LiquidityManager immutable lpManager;
 
     uint256 public totalOrders = 1;
 
     // orderId -> orders
     mapping(uint256 => Order) private orders;
 
-    constructor() {}
+    constructor(address _lpManager) {
+        lpManager = LiquidityManager(_lpManager);
+    }
 
     // --------- Events ---------
     event Orderbook__MarketOrderPlaced(
@@ -51,6 +61,16 @@ contract Orderbook {
             isFulfilled: false
         });
         orders[orderId] = marketOrder;
+
+        bool success = MockERC20(_inToken).transferFrom(
+            msg.sender,
+            address(lpManager),
+            _amount
+        );
+
+        if (!success) {
+            revert Orderbook__TokenTransferFailed();
+        }
 
         emit Orderbook__MarketOrderPlaced(
             orderId,
@@ -94,15 +114,80 @@ contract Orderbook {
         );
     }
 
+    function matchOrder(
+        uint256 _orderId,
+        uint256 _lpId,
+        address _tradingToken
+    ) public {
+        Order memory order = getOrderById(_orderId);
+
+        LiquidityGroup memory lp = lpManager.getLiquidityPosition(_lpId);
+
+        LiquidityToken memory tradingToken = lpManager.getTradingToken(
+            _lpId,
+            _tradingToken
+        );
+
+        uint256 currPrice = getTokenPrice(_tradingToken);
+
+        if (order.outToken == _tradingToken) {
+            if (
+                currPrice <= tradingToken.maxPrice &&
+                currPrice >= tradingToken.minPrice &&
+                order.amountOut <= tradingToken.availableBalance
+            ) {
+                lpManager.updateLiquidityPosition(
+                    _lpId,
+                    order.amountOut,
+                    true,
+                    false,
+                    _tradingToken
+                );
+                bool isPrimary = order.inToken == lp.primaryToken.token;
+                lpManager.updateLiquidityPosition(
+                    _lpId,
+                    order.amountIn,
+                    false,
+                    isPrimary,
+                    order.inToken
+                );
+            } else {
+                revert Orderbook__InvalidMatching();
+            }
+        } else {
+            if (
+                currPrice <= lp.primaryToken.maxPrice &&
+                currPrice >= lp.primaryToken.minPrice &&
+                order.amountOut <= lp.primaryToken.availableBalance
+            ) {
+                lpManager.updateLiquidityPosition(
+                    _lpId,
+                    order.amountOut,
+                    true,
+                    true,
+                    _tradingToken
+                );
+                bool isPrimary = order.inToken == lp.primaryToken.token;
+                lpManager.updateLiquidityPosition(
+                    _lpId,
+                    order.amountIn,
+                    false,
+                    isPrimary,
+                    order.inToken
+                );
+            } else {
+                revert Orderbook__InvalidMatching();
+            }
+        }
+    }
+
     // --------- Getter Functions ---------
 
     function getTokenPrice(address _token) private pure returns (uint256) {
         return 1000;
     }
 
-    function getMarketOrderById(
-        uint256 _orderId
-    ) public view returns (Order memory) {
+    function getOrderById(uint256 _orderId) public view returns (Order memory) {
         return orders[_orderId];
     }
 }
